9f0a30ff19adb2a8f77d1955c76e0159facc66d9
XinyeJiang <43395087+XinyeJiang@users.noreply.github.com>
2018-10-13 10:05:48 +0800

    Update correlation.js

--- lab4/correlation.js
+++ lab4/correlation.js
@@ -1,6 +1,6 @@
 //Data already collected
 //Index in each array corresponds to the Order Number
-//So the first order is seasonalDrinkNum[0] and numberOfPeopleInOrder[0]
+//So the first order is seasonalDrinkNum[0] and numberOfPeopleInOrder[1]
 var seasonalDrinkNum=[0,1,1,4,2,0,1,2,2,4,0,1,1,0,2,1,0,0,2,2]
 var numberOfPeopleInOrder=[1,2,3,5,2,3,2,1,3,6,2,1,3,1,4,2,1,2,1,4]
 
